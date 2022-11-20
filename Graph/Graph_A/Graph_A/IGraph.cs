63cb05ba2d6f87fa9eb93beaf453f01dde934e74
Henry Tu <67282986+hungtp2912@users.noreply.github.com>
2022-11-20 10:48:22 -0600

    Finish Vertex Class

---
+++ Graph/Graph_A/Graph_A/IGraph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_A
+{
+    public delegate void VisitorDelegate<T>(T value);
+    public interface IGraph<T> where T : IComparable<T>
+    {
+        #region Properties
+        int NumVertieces { get; }
+        int NumEdges { get; }
+        #endregion
+
+        #region Methods to work with vertices
+        void AddVertex(T data);
+        void RemoveVertex(T data);
+        bool HasVertex(T data);
+        Vertex<T> GetVertex(T data);
+        #endregion
+    }
+}
---
+++ Graph/Graph_A/Graph_A/Vertex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_A
+{
+    public class Vertex<T>: IComparable<Vertex<T>>
+        where T : IComparable<T>
+    {
+        #region Attributes
+        private T data;
+        private int index;
+        #endregion
+
+        #region Constructor
+        public Vertex(T data, int index)
+        {
+            this.data = data;
+            this.index = index;
+        }
+        #endregion
+
+        #region Properties
+        public T Data { get { return data; } }
+        public int Index { get { return index; } set { index = value; } }
+        #endregion
+
+        public int CompareTo(Vertex<T> other)
+        {
+            return Data.CompareTo(other.Data);
+        }
+
+        public override string ToString()
+        {
+            return "[" + data + "(" + index + ")]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Data.CompareTo(((Vertex<T>)obj).Data) == 0;
+        }
+    }
+}
