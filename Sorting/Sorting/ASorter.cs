ef4f14559fa3b2dd5a9f257f331b13d5d56cc49f
Henry <67282986+hungtp2912@users.noreply.github.com>
2022-12-11 14:34:36 -0600

    Create a Sorting Project, and add abstract class for this project

---
+++ Sorting/Sorting/ASorter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public abstract class ASorter<T>
+        where T: IComparable<T>
+    {
+        protected T[] array;
+        public ASorter(T[] array) => this.array = array;
+        public abstract void Sort();
+        protected void Swap(int first, int second)
+        {
+            T temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+        //How to implement [] on a class
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+        public int Length
+        {
+            get { return array.Length; }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder("[");
+            foreach (T item in array)
+            {
+                sb.Append(item);
+                sb.Append(', ');
+            }
+            if(sb.Length > 0)
+            {
+                sb.Remove(sb.Length - 2, 2);
+            }
+            sb.Append(']');
+            return sb.ToString();
+        }
+    }
+}
